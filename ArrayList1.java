7ee4339c241cbfd657b37cdfc245beae941b3bb9
Anjali Singh <anjalisingh81095@gmail.com>
2025-06-23 21:48:36 +0530

    h4

---
+++ ArrayList1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ArrayList1 {
+    public static void main (String [] args){
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(5);
+        
+        }
+}
