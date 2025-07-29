63e313a49460f0eb7fabf113b987901e380c3290
Anjali Singh <anjalisingh81095@gmail.com>
2025-07-29 23:32:13 +0530

    Sa

--- function/Function1.java
+++ function/Function1.java
@@ -7,7 +7,7 @@ public class Function1 {
        int sum = a+b;
 return sum;
     }
-    public static void main(String [] args){
+        public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b= sc.nextInt();
